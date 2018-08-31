a7d982ea42bd17cd87bce91a8ebf4f5eb8126fb5
Zichen Wang <wzc1995@pku.edu.cn>
2018-08-31 17:43:10 -0400

    revbyteorder v3 bug fixed

--- xinu-fall2018/system/main.c
+++ xinu-fall2018/system/main.c
@@ -25,9 +25,9 @@ process	main(void)
     /* Test of function revbyteorder */
     long x = 10;
 	kprintf("\noriginal: %d\n", x)
-	kprintf("version 1: %d\n", x, revbyteorder(x));
-	kprintf("version 2: %d\n", x, revbyteorder_inline(x));
-	kprintf("version 3: %d\n", x, revbyteorder_c(x));
+	kprintf("version 1: %d\n", revbyteorder(x));
+	kprintf("version 2: %d\n", revbyteorder_inline(x));
+	kprintf("version 3: %d\n", revbyteorder_c(x));
 
 
 	/* Run the Xinu shell */
