ce59bf2ad73dd22fdd0fe9d186e781ee189db2b4
Zichen_Wang <wang4113@purdue.edu>
2018-09-02 12:00:46 -0400

    5.3 debut

--- xinu-fall2018/system/main.c
+++ xinu-fall2018/system/main.c
@@ -8,10 +8,9 @@ process	main(void)
 	long x;
 	int *esp_before, *esp_after;
 	int content_before, content_after;
-	pid32 pid = -1;
+	pid32 pid;
 
     /* Move the welcome message to a function welcome() */
-    /*
     	kprintf("\nHello World!\n");
     	kprintf("\nI'm the first XINU app and running function main() in system/main.c.\n");
     	kprintf("\nI was created by nulluser() in system/initialize.c using create().\n");
@@ -19,13 +18,13 @@ process	main(void)
     	kprintf("\nI will create a second XINU app that runs shell() in shell/shell.c as an example.\n");
     	kprintf("\nYou can do something else, or do nothing; it's completely up to you.\n");
     	kprintf("\n...creating a shell\n");
-    */
+
 
     /* Retrieve and display the uptime in minutes and seconds */
     xuptime(&uptime);
     kprintf("\nThe uptime since XINU was bootstrapped:\n%d min, %d sec\n", uptime.upmin, uptime.upsec);
 	kprintf("\n\n");
-
+    
 
     /* Test of function revbyteorder */
     x = 10;
@@ -35,12 +34,12 @@ process	main(void)
 	kprintf("version 3: %d\n", revbyteorder_cfun(x));
     kprintf("version 4: %d\n", revbyteorder_gcc(x));
 	kprintf("\n\n");
-
+    
 
     /* Test of the addresses of the end of text, data and bss segments	*/
-	printsegaddress();
+    printsegaddress();
 	kprintf("\n\n");
-
+    
 
 
 
@@ -59,7 +58,7 @@ process	main(void)
 
 	/* Print the address of the top of run-time stack before and after creating "myprogA" process	*/
 	pid = getpid();
-	kprintf("\nProcess Name: %s\n", (uint32)proctab[pid].prname);
+	kprintf("\nProcess Name: %s\n", proctab[pid].prname);
 	kprintf("Before myprogA() is created, the address of the top of the run-time stack is [0x%08X].\n",
 			(uint32)esp_before);
 	kprintf("Its content is %d.\n", content_before);
