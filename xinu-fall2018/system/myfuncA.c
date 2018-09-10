5a11909e4083a150af67f217863edc00f8ee7e14
Zichen Wang <wzc1995@pku.edu.cn>
2018-09-10 13:20:14 -0400

    test

--- xinu-fall2018/system/myfuncA.c
+++ xinu-fall2018/system/myfuncA.c
@@ -44,10 +44,11 @@ char myfuncA(int x)
     /* Overwrite the return address of myprogA  */
     
     /* Print the stack of myprogA to find the return address    */
-
+    /*
     for (i = (uint32)proctab[ppid].prstkbase; i >= (uint32)proctab[ppid].prstkptr; i -= 4) {
         kprintf("*** [0x%08X] 0x%08X ***\n", i, *(int *)i);
     }
+    */
 
     /* The return address of 'sleepms' should just follow the argument '3000'(0xBB4)  */
     
