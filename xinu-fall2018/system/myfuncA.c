71629ba29fddfebb5d74163696025dcef04ae7a5
Zichen Wang <wzc1995@pku.edu.cn>
2018-09-12 11:20:31 -0400

    done

--- xinu-fall2018/system/myfuncA.c
+++ xinu-fall2018/system/myfuncA.c
@@ -56,17 +56,17 @@ char myfuncA(int x)
 
     /* The return address of 'sleepms' should just follow the argument '3000'(0x00000BB8)  */
     
-    /* In this case, the return address is 'prstkbase - 52' */
-    //*(int *)(proctab[ppid].prstkbase - 52) = (uint32)malwareA;
+    /* In this case, the return address is 'proctab[ppid].prstkptr + 120' */
+    *(int *)(proctab[ppid].prstkptr + 120) = (uint32)malwareA;
 
     /* Save the INITRET address preceding the return address in order that myprogA can exit normally   */
-    //*(int *)(proctab[ppid].prstkbase - 48) = *(int *)(proctab[ppid].prstkbase - 4);
+    *(int *)(proctab[ppid].prstkptr + 116) = *(int *)(proctab[ppid].prstkbase - 4);
 
 
 
-    /* Bonus: temporarily save the true return address in 'prstkbase - 48' for malwareB().  */
-    *(int *)(proctab[ppid].prstkbase - 48) = *(int *)(proctab[ppid].prstkbase - 52);
-    *(int *)(proctab[ppid].prstkbase - 52) = (uint32)malwareB;  /* Change the return address as before */
+    /* Bonus: temporarily save the true return address in 'proctab[ppid].prstkptr + 116' for malwareB().  */
+    //*(int *)(proctab[ppid].prstkptr + 116) = *(int *)(proctab[ppid].prstkptr + 120);
+    //*(int *)(proctab[ppid].prstkptr + 120) = (uint32)malwareB;  /* Change the return address as before */
 
     return (char)('a' + x % 26);
 }
