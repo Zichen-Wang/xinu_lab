70ac30e36b06dc00c8e007dfc267e608d9125dd1
Zichen Wang <wzc1995@pku.edu.cn>
2018-09-19 23:22:00 -0400

    4 test

--- xinu-fall2018/system/myTestProc.c
+++ xinu-fall2018/system/myTestProc.c
@@ -12,9 +12,10 @@ process myTestProc(void)
     pid32   pid;          /* Used in Lab2 4   */
     pid = igetpid();      /* Lab2 4: Get current process id by igetpid()  */
 
+    kprintf("\n")
     kprintf("Process ID: %d\n", pid);
     kprintf("Process Name: %s\n", proctab[pid].prname);
-    kprintf("\n\n");
+    kprintf("\n");
 
     return 0;
 }
