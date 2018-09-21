6b724ed7bb1f201292d534a2f207491f40744f18
Zichen Wang <wzc1995@pku.edu.cn>
2018-09-20 21:04:17 -0400

    5.5 debug

--- xinu-fall2018/system/appR3test.c
+++ xinu-fall2018/system/appR3test.c
@@ -26,7 +26,7 @@ void appR3test(int proctype)
     kprintf("Process Type: %s\n", (proctype == 0) ? "CPU-bound" : "IO-bound");
     kprintf("Clktimemilli: %d ms\n", clktimemilli);
     kprintf("Gross CPU usage: %d ms\n", proctab[pid].pgrosscpu);
-    kprintf("Average waiting time: %.2lf ms\n", proctab[pid].pwaittime / proctab[pid].pwaitcount);
+    kprintf("Average waiting time: %f ms\n", 1.0 * proctab[pid].pwaittime / proctab[pid].pwaitcount);
     kprintf("\n");
 
 }
