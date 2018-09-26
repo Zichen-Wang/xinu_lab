2cce5e3b714f41c936aaaf6aa99dd000c7d26e8b
Zichen Wang <wzc1995@pku.edu.cn>
2018-09-25 21:29:02 -0400

    5.5 test

--- xinu-fall2018/system/appR3test.c
+++ xinu-fall2018/system/appR3test.c
@@ -27,9 +27,16 @@ void appR3test(int proctype)
     // Print PID, proctype, clktimemilli, gross CPU usage, and average time before terminating.
     pid = igetpid();
 
+    /*
     kprintf("\nProcess ID: %d\n", pid);
     kprintf("Process Type: %s\n", (proctype == 0) ? "CPU-bound" : "IO-bound");
     kprintf("Gross CPU usage: %d ms, average waiting time: %f ms\n\n",
+            proctab[pid].pgrosscpu + currproctime, 1.0 * proctab[pid].pwaittime / proctab[pid].pwaitcount);
+    */
+
+    kprintf("\n%d\n", pid);
+    kprintf("%d\n", proctype);
+    kprintf("%d, %f\n",
             proctab[pid].pgrosscpu + currproctime, 1.0 * proctab[pid].pwaittime / proctab[pid].pwaitcount);
 
 }
