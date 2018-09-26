fd9f1757507304e790c057808d6e662f160221d2
Zichen Wang <wzc1995@pku.edu.cn>
2018-09-26 01:14:40 -0400

    5.5 test

--- xinu-fall2018/system/igetpid.c
+++ xinu-fall2018/system/igetpid.c
@@ -16,11 +16,9 @@ int igetpid(void)
 {
     int syscall_val;
 
-    asm volatile ("pushl  %%eax\n\t"        /* Save the old EAX value in the user stack  */
-                  "movl   $3, %%eax\n\t"    /* Set syscall #3 to EAX            */
+    asm volatile ("movl   $3, %%eax\n\t"    /* Set syscall #3 to EAX            */
                   "int    $0x21\n\t"        /* Trap to interrupt table #33      */
-                  "movl   %%eax, %0\n\t"    /* Save the return value to the variable syscall_vall   */
-                  "popl   %%eax\n\t"        /* Restore the value of EAX         */
+                  "movl   %%eax, %0\n\t"    /* Save the return value to the local variable syscall_vall   */
                 : "=r" (syscall_val)        /* Extended inline assembly: syscall_vall is an output variable */
                 :
                 : "%eax");                  /* Tell the gcc that we used EAX in this inline assembly   */
