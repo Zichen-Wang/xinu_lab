7a7a71ca2e0a648a40b84ea6768694b2c94d3074
Zichen Wang <wzc1995@pku.edu.cn>
2018-09-25 20:51:23 -0400

    5.5 test

--- xinu-fall2018/system/main.c
+++ xinu-fall2018/system/main.c
@@ -185,7 +185,7 @@ process	main(void)
 
     /* Lab2 5.5: Scenario 2: create 8 app processes that are all IO-bound  */
     sleepms(10);    /* Lab2 5.5: Make sure "Main" process has the highest priority when creating apps		*/
-    /*
+
     kprintf("\nScenario 2: create 8 app processes that are all I/O-bound.\n");
     resume(create(appR3test, 1024, INITPRIO, "appR3test", 1, 1));
     resume(create(appR3test, 1024, INITPRIO, "appR3test", 1, 1));
@@ -195,8 +195,8 @@ process	main(void)
     resume(create(appR3test, 1024, INITPRIO, "appR3test", 1, 1));
     resume(create(appR3test, 1024, INITPRIO, "appR3test", 1, 1));
     resume(create(appR3test, 1024, INITPRIO, "appR3test", 1, 1));
-    */
-    //sleepms(25000);      /* Lab2 5.5: "Main" waits for these "appR3test" processes		*/
+
+    sleepms(25000);      /* Lab2 5.5: "Main" waits for these "appR3test" processes		*/
 
     /* Lab2 5.5: Scenario 3: create 4 app processes that are CPU-bound and 4 app processes that are I/O-bound  */
     sleepms(10);    /* Lab2 5.5: Make sure "Main" process has the highest priority when creating apps		*/
