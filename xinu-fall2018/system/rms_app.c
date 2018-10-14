92ff4c5a7997006a475d5c6cba102a352adbfd97
Zichen Wang <wzc1995@pku.edu.cn>
2018-10-13 23:26:40 -0400

    lab4

--- xinu-fall2018/system/rms_app.c
+++ xinu-fall2018/system/rms_app.c
@@ -14,11 +14,11 @@
 
 void rms_app(uint32 x, uint32 y)    /* x specifies computation time requirement and y specifies period  */
 {
-    uint32  period_start;       /* remember the current time given by clktimemilli     */
-    uint32  comp_received;      /* track CPU time received by the process in the period    */
-    uint32  comp_temp;          /* remember the value of currproctime
-                                 * which will otherwise be lost upon context-switch.    */
+    uint32  period_start;       /* remember the current time given by clktimemilli      */
+    uint32  grosscpu_start;     /* track the gross CPU usage at the start of period     */
     uint32  period_number;      /* track the number of period   */
+    uint32  comp_received;      /* track CPU time received by the process in the period    */
+
     pid32   pid = getpid();      /* get the pid of this process  */
 
     uint32  sleeptime;          /* time to sleep    */
@@ -26,31 +26,26 @@ void rms_app(uint32 x, uint32 y)    /* x specifies computation time requirement 
     for (period_number = 1; period_number <= 100; period_number++) {     /* Outer loop   */
 
         period_start = clktimemilli;    /* Remember the current time    */
-
-        comp_received = 0;              /* Initialize CPU time received by the process in the period to zero    */
-        comp_temp = currproctime;       /* Initialize the current value of currproctime */
+        grosscpu_start = proctab[currpid].pgrosscpu;    /* Remember the gross CPU usage at the start of period */
+        comp_received = 0;          /* Initialize computation time to zero  */
 
         while (1) { /* Inner loop   */
-            if (currproctime == 0) {  /* Context-switch has occurred  */
-                comp_received += comp_temp;
-                comp_temp = 0;
-            }
-            comp_temp = currproctime;   /* Update the running time of current process in case of preemption  */
-            if (clktimemilli - period_start < y) {  /* The process is still in this period  */
-                if (comp_received + currproctime >= x) {           /* Check whether current computation time has reached x  */
+            /* Update the computation time  */
+            comp_received = proctab[currpid].pgrosscpu + currproctime - grosscpu_start;
 
+            if (clktimemilli - period_start < y) {  /* The process is still in this period  */
+                if (comp_received >= x) {           /* Check whether current computation time has reached x  */
                     sleeptime = y - (clktimemilli - period_start);  /* Calculate the sleep time */
 
                     kprintf("%d\t%d\t%d\t%d\t%d\t%d\n",
                             pid, x, y, period_number, clktimemilli, sleeptime);
 
                     sleepms(sleeptime);     /* The real-time process has finished   */
-
                     break;                                          /* Go to the next period    */
                 }
             }
             else {                                  /* This period has expired  */
-                if (comp_received + currproctime < x) {           /* The real-time process has not finished in time  */
+                if (comp_received < x) {           /* The real-time process has not finished in time  */
 
                     kprintf("%d\t%d\t%d\t%d\t%d\t%d\t[deadline violation]\n",
                             pid, x, y, period_number, clktimemilli, y - (clktimemilli - period_start));
