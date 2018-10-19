f8ef1135bbcc3b2d20a701fac9a036b1c0df4b2b
Zichen Wang <wzc1995@pku.edu.cn>
2018-10-18 23:21:32 -0400

    lab4 3

--- xinu-fall2018/system/main.c
+++ xinu-fall2018/system/main.c
@@ -338,11 +338,15 @@ process	main(void)
 	 * User: wang4113
 	 * date: 10/18/2018
 	 */
+	/* Lab4 3: Test IPC	*/
+	proctab[currpid].prprio = MAXPRIO;
 
     resume(create(test_receiver, 1024, INITPRIO, "test_receiver_1", 0));
-	resume(create(test_receiver, 1024, INITPRIO, "test_receiver_2", 0));
-	resume(create(test_receiver, 1024, INITPRIO, "test_receiver_3", 0));
-    resume(create(test_sender, 1024, INITPRIO, "test_sender_1", 2, 4, 3));
+	//resume(create(test_receiver, 1024, INITPRIO, "test_receiver_2", 0));
+	//resume(create(test_receiver, 1024, INITPRIO, "test_receiver_3", 0));
+    resume(create(test_sender, 1024, INITPRIO, "test_sender_1", 2, 4, 1));
+
+	proctab[currpid].prprio = INITPRIO;
 
     while (TRUE) {
     	;
