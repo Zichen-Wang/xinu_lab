9b5aa78dc8616e824f9dbfed293c3d98de268fb5
Zichen Wang <wzc1995@pku.edu.cn>
2018-10-22 13:31:50 -0400

    lab4 4

--- xinu-fall2018/system/main.c
+++ xinu-fall2018/system/main.c
@@ -365,9 +365,9 @@ process	main(void)
 
 	proctab[currpid].prprio = MAXPRIO;
 
-	resume(create(test_xalarm, 1024, INITPRIO, "test_xalarm_1", 1, 20));
-	resume(create(test_xalarm, 1024, INITPRIO, "test_xalarm_2", 1, 70));
-	resume(create(test_xalarm, 1024, INITPRIO, "test_xalarm_3", 1, 120));
+	resume(create(test_xalarm, 1024, INITPRIO, "test_xalarm_1", 1, 45));
+	resume(create(test_xalarm, 1024, INITPRIO, "test_xalarm_2", 1, 100));
+	resume(create(test_xalarm, 1024, INITPRIO, "test_xalarm_3", 1, 150));
 
 	proctab[currpid].prprio = INITPRIO;
 
