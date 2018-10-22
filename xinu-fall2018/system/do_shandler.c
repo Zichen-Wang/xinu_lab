d2d2587da1b299c2348973e8dbf80038cac0380a
Zichen Wang <wzc1995@pku.edu.cn>
2018-10-22 00:15:36 -0400

    lab4 4

--- xinu-fall2018/system/do_shandler.c
+++ xinu-fall2018/system/do_shandler.c
@@ -3,7 +3,7 @@
  * date: 10/17/2018
 */
 
-/* do_handler.c - do_shandler */
+/* do_shandler.c - do_shandler */
 
 #include <xinu.h>
 
@@ -14,36 +14,4 @@
 
 void do_shandler(uint32 original_ret_addr)
 {
-    intmask	mask;			/* Saved interrupt mask		*/
-    struct	procent *prptr;		/* Ptr to process's table entry	*/
-
-    mask = disable();       /* Disable interrupts  */
-
-    prptr = &proctab[currpid];
-
-    if (((prptr -> prsig)[SIGRECV]).regyes == TRUE) {
-        asm volatile ("sti");		/* Enable interrupts	*/
-        (prptr -> prsig)[SIGRECV].fnt();	/* Call callback function for SIGRECV	*/
-        asm volatile ("cli");		/* Disable interrupts	*/
-    }
-
-    if (((prptr -> prsig)[SIGTIME]).regyes == TRUE
-     && ((prptr -> prsig)[SIGTIME]).optarg > 0
-     && ((prptr -> prsig)[SIGTIME]).optarg <= clktimemilli) {
-        /* when the scheduler decides to run this process,
-         * the alarm time should be non-zero and equal to or lower than clktimemilli    */
-
-        /* Clear the alarm  */
-        ((prptr -> prsig)[SIGTIME]).optarg = 0;
-
-        asm volatile ("sti");		/* Enable interrupts	*/
-        (prptr -> prsig)[SIGTIME].fnt();	/* Call callback function for SIGTIME	*/
-        asm volatile ("cli");		/* Disable interrupts	*/
-
-    }
-
-    /* modify the return address which is at prptr -> prstkptr + 44 to original_ret_addr	*/
-    *(int *)(prptr -> prstkptr + 44) = original_ret_addr;
-
-    restore(mask);          /* Restore mask    */
 }
