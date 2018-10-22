63548b3517d470fc6b7128477bf1786b74b1c2d9
Zichen Wang <wzc1995@pku.edu.cn>
2018-10-22 18:48:49 -0400

    lab4 4

--- xinu-fall2018/system/main.c
+++ xinu-fall2018/system/main.c
@@ -380,9 +380,17 @@ process	main(void)
 	resume(create(test_xcpu, 1024, INITPRIO, "test_xcpu_2", 1, 750));
 	resume(create(test_xcpu, 1024, INITPRIO, "test_xcpu_3", 1, 1200));
 
+	resume(create(test_xcpu, 1024, INITPRIO, "test_xcpu_1", 1, 1200));
+	resume(create(test_xcpu, 1024, INITPRIO, "test_xcpu_2", 1, 2750));
+	resume(create(test_xcpu, 1024, INITPRIO, "test_xcpu_3", 1, 3200));
+
 	resume(create(test_xalarm, 1024, INITPRIO, "test_xalarm_1", 1, 1000));
 	resume(create(test_xalarm, 1024, INITPRIO, "test_xalarm_2", 1, 1000));
 	resume(create(test_xalarm, 1024, INITPRIO, "test_xalarm_3", 1, 1000));
+
+	resume(create(test_xalarm, 1024, INITPRIO, "test_xalarm_1", 1, 5000));
+	resume(create(test_xalarm, 1024, INITPRIO, "test_xalarm_2", 1, 5000));
+	resume(create(test_xalarm, 1024, INITPRIO, "test_xalarm_3", 1, 5000));
 
 	resume(create(test_sender, 1024, INITPRIO, "test_sender_1", 2, 4, 6));
 
