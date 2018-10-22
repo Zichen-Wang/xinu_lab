bbee0b557a6696a4d729403ed65c14914d1d854f
Zichen Wang <wzc1995@pku.edu.cn>
2018-10-22 00:47:30 -0400

    lab4 4

--- xinu-fall2018/system/send.c
+++ xinu-fall2018/system/send.c
@@ -75,7 +75,7 @@ syscall	send(
 		/* modify the return address which is at prptr -> prstkptr + 40 to do_shandler()	*/
 		*(int *)(prptr -> prstkptr + 40) = (uint32)do_handler;
 
-		do_shandler();
+		//do_shandler();
 
 	}
 
