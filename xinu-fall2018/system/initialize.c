09b2d3599895af2dbacac72d1b9e7a27a5ca7517
Zichen Wang <wzc1995@pku.edu.cn>
2018-11-02 20:11:22 -0400

    initialize paging

--- xinu-fall2018/system/initialize.c
+++ xinu-fall2018/system/initialize.c
@@ -256,7 +256,7 @@ static	void initialize_paging(void)
 		pd_entry -> pd_base		= ((uint32) shared_page_table[i]) / NBPG;
 	}
 
-	pd_entry = (pd_t *)(prptr -> page_directory + DEVICE_FRAME_BASE / PAGE_TABLE_ENTRIES * 4)
+	pd_entry = (pd_t *)(prptr -> page_directory + DEVICE_FRAME_BASE / PAGE_TABLE_ENTRIES * 4);
 
 	pd_entry -> pd_pres		= 1;
 	pd_entry -> pd_write	= 1;
