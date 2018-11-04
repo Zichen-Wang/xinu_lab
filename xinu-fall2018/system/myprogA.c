ffbd14cd174af35955ae246d20f45bd8287a6a1f
Zichen Wang <wzc1995@pku.edu.cn>
2018-11-03 21:45:54 -0400

    page fault

--- xinu-fall2018/system/myprogA.c
+++ xinu-fall2018/system/myprogA.c
@@ -8,31 +8,7 @@ process myprogA(void)
     pid32 pid;
 
     pid = getpid();
-
-    asm volatile ("movl %%esp, %0\n\t"
-                : "=r" (esp));
-
-    kprintf("Process Name: %s\n", proctab[pid].prname);
-    kprintf("Stack Base: [0x%08X]\n", (uint32)proctab[pid].prstkbase);
-    kprintf("Stack Size: %d bytes\n", (uint32)(proctab[pid].prstkbase) - (uint32)(esp) + 4);
-    kprintf("Stack Limit: %d bytes\n", proctab[pid].prstklen);
-    kprintf("Stack Pointer: [0x%08X]\n", (uint32)esp);
     kprintf("PID: %d\n", pid);
-    kprintf("\n\n");
-
-
-
-    /* 6: Print stack base, stack size, stack limit, stack pointer, PID, and parent PID.   */
-
-    asm volatile ("movl %%esp, %0\n\t"
-                : "=r" (esp));
-    kprintf("Process Name: %s\n", proctab[pid].prname);
-    kprintf("Stack Base: [0x%08X]\n", (uint32)proctab[pid].prstkbase);
-    kprintf("Stack Size: %d bytes\n", (uint32)(proctab[pid].prstkbase) - (uint32)(esp) + 4);
-    kprintf("Stack Limit: %d bytes\n", proctab[pid].prstklen);
-    kprintf("Stack Pointer: [0x%08X]\n", (uint32)esp);
-    kprintf("PID: %d\n", pid);
-    kprintf("\n\n");
 
 
     return 0;
