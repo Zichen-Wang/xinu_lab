cfee012d2123d427419f0090add151e152d72768
Zichen Wang <wzc1995@pku.edu.cn>
2018-11-03 23:10:54 -0400

    page fault

--- xinu-fall2018/system/free_frames.c
+++ xinu-fall2018/system/free_frames.c
@@ -15,42 +15,41 @@
 void free_frames(pid32 pid)
 {
     struct	procent	*prptr;     /* Ptr to process table entry	*/
-    pd_t    *pd_entry;          /* Ptr to page directory entry	*/
-    pt_t    *pt_entry;          /* Ptr to page table entry	*/
+    pd_t    *pd;                /* Ptr to page directory	*/
+    pt_t    *pt;                /* Ptr to page table	*/
     int     frame_pd_num, frame_pt_num, frame_virt_num;
     uint32  i, j;
 
     prptr = &proctab[pid];
 
+    pd = (pd_t *)(prptr -> page_directory);
     for (i = 0; i < PAGE_DIRECTORY_ENTRIES; i++) {
 
-        pd_entry = (pd_t *)(prptr -> page_directory + i * 4);
-
         /* the page table is present */
-        if (pd_entry -> pd_pres == 1) {
-            pd_entry -> pd_pres = 0;
+        if (pd[i].pd_pres == 1) {
             /* Find the frame number of this page table   */
-            frame_pt_num = (pd_entry -> pd_base) - FRAME0;
+            frame_pt_num = (pd[i].pd_base) - FRAME0;
 
             /* it is not a shared page table   */
             if (inverted_page_table[frame_pt_num].fstate != F_SHARED_PT) {
 
+                pt = (pt_t *)(NBPG * pd[i].pd_base);
                 for (j = 0; j < PAGE_TABLE_ENTRIES; j++) {
-                    pt_entry = (pt_t *)(NBPG * (pd_entry -> pd_base) + j * 4);
 
                     /* the true frame is present  */
-                    if (pt_entry -> pt_pres == 1) {
-                        pt_entry -> pt_pres = 0;
+                    if (pt[i].pt_pres == 1) {
                         /* Find the frame number of the virtual page   */
-                        frame_virt_num = (pt_entry -> pt_base) - FRAME0;
+                        frame_virt_num = (pt[i].pt_base) - FRAME0;
 
                         /* Free this frame for virtual memory */
                         inverted_page_table[frame_virt_num].fstate = F_FREE;
+                        pt[i].pt_pres = 0;
                     }
                 }
 
                 /* Free this frame of page table */
                 inverted_page_table[frame_pt_num].fstate = F_FREE;
+                pd[i].pd_pres = 0;
 
                 hook_ptable_delete(frame_pt_num);
             }
