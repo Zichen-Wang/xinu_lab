9c786fcacc7594c469d25f52904d44407d6dc2f7
Zichen Wang <wzc1995@pku.edu.cn>
2018-11-04 16:19:26 -0500

    vgetmem & vfreemem debug

--- xinu-fall2018/system/myprogA.c
+++ xinu-fall2018/system/myprogA.c
@@ -13,7 +13,7 @@ process myprogA(void)
     for (t = 0; t < 1000; t++) {
         array = (int *)vgetmem(10000);
         for (i = 0; i < 2500; i++) {
-            array[i] = i * pid;
+            array[i] = t * i;
         }
 
         kprintf("\npid %d, 1348th %d\n", pid, array[1348]);
