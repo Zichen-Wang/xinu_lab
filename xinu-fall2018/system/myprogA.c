b27b991c69d68c223be75a54c8af98f675fccf29
Zichen Wang <wzc1995@pku.edu.cn>
2018-11-03 23:42:34 -0400

    page fault

--- xinu-fall2018/system/myprogA.c
+++ xinu-fall2018/system/myprogA.c
@@ -5,12 +5,17 @@
 process myprogA(void)
 {
     pid32   pid;
-    int     *array = NULL;
+    int *array;
 
     pid = getpid();
 
-    //array = (int *)(getmem(40));
-    printf("\n%d: [0x%08X]\n", pid, array);
+    array = (int *)(getmem(40));
+    for (int i = 0; i < 10; i++)
+        array[i] = pid * i;
+
+    for (int i = 0; i < 10; i++)
+        kprintf("\npid %d: %d\n", pid, array[i]);
+
 
     return 0;
 }
