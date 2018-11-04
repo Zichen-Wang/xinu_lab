7817e5d8cde57c3b6ab55ee173b006d0a21f30db
Zichen Wang <wzc1995@pku.edu.cn>
2018-11-03 21:59:00 -0400

    page fault

--- xinu-fall2018/system/myprogA.c
+++ xinu-fall2018/system/myprogA.c
@@ -4,11 +4,14 @@
 
 process myprogA(void)
 {
-    int *esp;
-    pid32 pid;
+    pid32   pid;
+    uint32  cr3;
+
+    asm volatile ("movl %%cr3, %0"
+                : "=r" (cr3));
 
     pid = getpid();
-    kprintf("\n%d: 0x%08X\n", pid, proctab[pid].page_directory);
+    kprintf("\n%d 0x%08X 0x%08X\n", pid, proctab[pid].page_directory, cr3);
 
 
     return 0;
