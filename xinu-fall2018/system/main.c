43c2817b38044510eadc726ed364848eaf01982a
Zichen Wang <wzc1995@pku.edu.cn>
2018-11-18 19:00:40 -0500

    test

--- xinu-fall2018/system/main.c
+++ xinu-fall2018/system/main.c
@@ -21,6 +21,9 @@ process	main(void)
 	resume(vcreate(test_vm, 1024, 100, 19, "test_vm", 1, 'b'));
 	resume(vcreate(test_vm, 1024, 100, 19, "test_vm", 1, 'c'));
 
+	resume(vcreate(test_vm_2, 1024, 100, 19, "test_vm_2", 2, 1, 2));
+	resume(vcreate(test_vm_2, 1024, 100, 19, "test_vm_2", 2, 23, 17));
+
 	sleepms(100);
 
 	kprintf("\n...creating a shell\n");
