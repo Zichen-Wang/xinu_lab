380c6c86a2bd9f3c9df2c396828a585442b9255e
Zichen Wang <wzc1995@pku.edu.cn>
2018-11-25 15:13:08 -0500

    lab 6 debug

--- xinu-fall2018-paging/system/test_vm.c
+++ xinu-fall2018-paging/system/test_vm.c
@@ -8,30 +8,36 @@ process test_vm(char c)
     int *array;
     uint32  i;
 
-    kprintf("before %d\n", *(int *)(2024 * 4096 + 8));
+    kprintf("before [2024 * 4096 + 8] = %d\n", *(int *)(2024 * 4096 + 8));
     kprintf("before pid: %d, frame num %d\n", currpid, proctab[currpid].page_directory[4].pd_base);
     *(int *)(2024 * 4096 + 8) = 9;
-    kprintf("before %d\n", *(int *)(0x01001008));
+    //kprintf("before %d\n", *(int *)(0x01001008));
     //kprintf("after pid: %d, frame num %d\n", currpid, proctab[currpid].page_directory[4].pd_base);
     *(int *)(0x01001008) = 10;
     kprintf("after pid: %d, frame num %d\n", currpid, proctab[currpid].page_directory[4].pd_base);
-    kprintf("after %d\n", *(int *)(2024 * 4096 + 8));
+    kprintf("after [2024 * 4096 + 8] = %d\n", *(int *)(2024 * 4096 + 8));
+    kprintf("after [0x01001008] = %d\n", *(int *)(0x01001008));
+    *(int *)(0x01001008) = 10;
+    *(int *)(0x01000000) = 1;
+    kprintf("after after [0x01001008] = %d\n", *(int *)(0x01001008));
+    kprintf("after after [2025 * 4096] = %d\n", *(int *)(2025 * 4096));
     kprintf("fault number %d\n", get_faults());
-    pid = getpid();
 
+    /*
+    pid = getpid();
 
     array = (int *)vgetmem(10000);
 
 
 
 
-    for (i = 1026; i >= 1024; i--) {
+    for (i = 2049; i >= 2048; i--) {
         array[i] = 10;
         kprintf("%d %d\n", i, array[i]);
     }
 
 
     vfreemem(array, 10000);
-
+    */
     return 0;
 }
