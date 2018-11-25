fc6476918f79f97ba9166a9af002fdb98df3a0ec
Zichen Wang <wzc1995@pku.edu.cn>
2018-11-25 02:06:32 -0500

    lab 6 debug

--- xinu-fall2018-paging/system/test_vm.c
+++ xinu-fall2018-paging/system/test_vm.c
@@ -5,20 +5,22 @@
 process test_vm(char c)
 {
     pid32   pid;
-    int *array;
+    char *array;
     uint32  i;
 
     pid = getpid();
 
     array = vgetmem(40000);
 
-    for (i = 0; i < 10000; i++) {
-        array[i] = (int)(c);
+    for (i = 0; i < 40000; i++) {
+        array[i] = c;
+        if (i % 4096 == 0)
+            kprintf("%d %d\n", i, array[i]);
     }
 
     kprintf("Process %d: array base address is [[0x%08X]]\n", pid, array);
-    for (i = 0; i < 10000; i += 1024)
-        kprintf("PID %d page %d: %d\n", pid, i / 1024, array[i]);
+    for (i = 0; i < 40000; i += 4096)
+        kprintf("PID %d page %d: %d\n", pid, i / 4096, array[i]);
     kprintf("\n");
 
     vfreemem(array, 40000);
