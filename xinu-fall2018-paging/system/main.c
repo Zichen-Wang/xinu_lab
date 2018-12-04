770f6d17e735f88b0a13a74090670ce8b68e0ebd
Zichen Wang <wzc1995@pku.edu.cn>
2018-12-04 00:13:44 -0500

    lab 6 debug

--- xinu-fall2018-paging/system/main.c
+++ xinu-fall2018-paging/system/main.c
@@ -18,12 +18,12 @@ process	main(void)
 	//resume(create(test_app, 8192, 19, "test_app", 1, 'c'));
 
 
-	resume(vcreate(test_vm, 8192, 100, 19, "test_vm", 1, 'a'));
-	//resume(vcreate(test_vm, 8192, 50, 19, "test_vm", 1, 'b'));
-	//resume(vcreate(test_vm, 8192, 50, 19, "test_vm", 1, 'c'));
+	resume(vcreate(test_vm, 8192, 50, 19, "test_vm", 1, 'a'));
+	resume(vcreate(test_vm, 8192, 50, 19, "test_vm", 1, 'b'));
+	resume(vcreate(test_vm, 8192, 50, 19, "test_vm", 1, 'c'));
 
-	//resume(vcreate(test_vm_2, 8192, 100, 19, "test_vm_2", 2, 1, 2));
-	//resume(vcreate(test_vm_2, 8192, 100, 19, "test_vm_2", 2, 23, 17));
+	resume(vcreate(test_vm_2, 8192, 100, 19, "test_vm_2", 2, 1, 2));
+	resume(vcreate(test_vm_2, 8192, 100, 19, "test_vm_2", 2, 23, 17));
 
 	sleepms(40000);
 	kprintf("faults number: %d\n", get_faults());
