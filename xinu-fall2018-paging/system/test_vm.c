56d2637c89abb265e072c427a93cd0cfbd1b472b
Zichen Wang <wzc1995@pku.edu.cn>
2018-12-04 18:35:02 -0500

    lab 6 debug

--- xinu-fall2018-paging/system/test_vm.c
+++ xinu-fall2018-paging/system/test_vm.c
@@ -38,13 +38,13 @@ process test_vm(char c)
 
 
     for (i = 0; i < 60 * 4096; i++) {
-        //array[i] = (i * 32 + c) % 13;
-        array[i] = c;
+        array[i] = (i * 32 + c) % 13;
+        //array[i] = c;
     }
 
     for (i = 0; i < 60 * 4096; i++) {
         //kprintf("%d: %d\n", i, array[i]);
-        if (array[i] != c) {
+        if (array[i] != (i * 32 + c) % 13) {
             vfreemem(array, 60 * 4096);
             send(2, -1);
             return 0;
