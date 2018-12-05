58aa4a01ad4909b081049386c4a512509263d041
Zichen Wang <wzc1995@pku.edu.cn>
2018-12-05 01:34:11 -0500

    lab 6 test

--- xinu-fall2018-paging/include/page_data_structure.h
+++ xinu-fall2018-paging/include/page_data_structure.h
@@ -100,7 +100,7 @@ extern  void enable_paging(void);
 extern  void pfisr(void);
 
 /* in file pfhandler.c  */
-extern  void pfhandler(void);
+extern  void pfhandler(int, int, int);
 
 /* in file test_app.c    */
 extern  process test_app(char);
