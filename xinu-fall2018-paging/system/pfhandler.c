e9ace98c2d215036252912991537185a153a3ec8
Zichen Wang <wzc1995@pku.edu.cn>
2018-12-05 01:45:00 -0500

    lab 6 test

--- xinu-fall2018-paging/system/pfhandler.c
+++ xinu-fall2018-paging/system/pfhandler.c
@@ -109,11 +109,6 @@ void	pfhandler(uint32 pferror_code)
     s = find_bs_map(vp, currpid);
     o = vp - backing_store_map[s].virt_base_num;
 
-    kprintf("Process ID %d is reading frame %d from s: %d, o: %d\n", currpid, f, s, o);
-    if (read_bs((char *)(NBPG * f), s, o) == SYSERR) {
-        kprintf("Process %d: Cannot read a page from backing store!\n", currpid);
-    }
-
 
     /* Update pt to mark the appropriate entry as present, and set other relevant fields */
 
@@ -136,6 +131,12 @@ void	pfhandler(uint32 pferror_code)
     pt[q].pt_avail  = 0;
 
     pt[q].pt_base   = f;
+
+
+    kprintf("Process ID %d is reading frame %d from s: %d, o: %d\n", currpid, f, s, o);
+    if (read_bs((char *)(NBPG * f), s, o) == SYSERR) {
+    kprintf("Process %d: Cannot read a page from backing store!\n", currpid);
+    }
 
 
 }
